37e5be7193fbbdf823b2c54e18258d8d2fa4b471
Eliza Czaplewska <czaplewska.eliza@gmail.com>
2021-12-06 22:38:17 +0100

    wyswietlanie

--- alfabet.py
+++ alfabet.py
@@ -12,17 +12,24 @@ letters = ['A','Ą','B','C','Ć','D','E','Ę','F','G','H','I','J','K','L','Ł','
 # assigning values to X and Y variable
 X = 400
 Y = 400
-
+def display_text(text):
+    text = font.render(text, True, green, blue)
+    pygame.time.delay(1000)
+    display_surface.blit(text, (X // 2, Y // 2))
+    pygame.display.update()
 display_surface = pygame.display.set_mode((X, Y))
 
 # set the pygame window name
 pygame.display.set_caption('Komunikacja Człowiek-Komputer')
 font = pygame.font.Font('freesansbold.ttf', 32)
-
+display_surface.fill(white)
 time_elapsed_since_last_action = 0
 clock = pygame.time.Clock()
 i=0
-
+display_text('3..')
+display_text('2..')
+display_text('1..')
+display_text('START')
 while True:
     display_surface.fill(white)
 
@@ -30,7 +37,7 @@ while True:
     time_elapsed_since_last_action += dt
 
     text = font.render(letters[i%len(letters)], True, green, blue)
-    pygame.time.delay(200)
+    pygame.time.delay(500)
     i += 1
     display_surface.blit(text, (X // 2, Y // 2))
     for event in pygame.event.get():
